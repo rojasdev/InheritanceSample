9031c8c4b8b49b8d32afee9f4c0f9738726893e3
rojasdev <r.rojas@usls.edu.ph>
2022-10-05 13:53:42 +0800

    Initial commit

---
+++ src/com/inheritance/sample/Main.java
@@ -0,0 +1,25 @@
+package com.inheritance.sample;
+
+public class Main extends Teacher{
+    protected Main() {
+        super("A","B","C");
+    }
+    public static void main(String[] args) {
+        Teacher obj = new Teacher("Teacher","CET","Programming");
+        //accessing the fields of parent class
+        System.out.println(obj.collegeName); // Retrieve as non private
+        System.out.println(obj.getDesignation()); // Retrieve as private using getter
+        System.out.println(obj.getSubjectName());
+
+        obj.setDesignation("Lecturer"); // Change value as private
+        System.out.println(obj.getDesignation());
+
+        System.out.println(obj.getSubjectName()); // from sub class
+
+        //accessing the method of parent class
+        obj.does();
+
+        // This will show an error due to private method
+        // obj.does("Private");
+    }
+}
